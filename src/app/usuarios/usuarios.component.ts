c1492c1c40b0ea1d5de215f55f58941a71f5eebf
Alexandre Oliveira Silva <alexandre-silva@spgg.rs.gov.br>
2025-06-09 09:34:47 -0300

    Ajuste na largura do menu e outros

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -4,6 +4,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 interface Usuario {
   id: number;
@@ -20,20 +21,28 @@ interface Usuario {
     MatTableModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule],
+    MatIconModule,
+  ],
   templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.css']
+  styleUrls: ['./usuarios.component.css'],
 })
 export class UsuariosComponent implements OnInit {
   colunas: string[] = ['id', 'name', 'email', 'role'];
   usuarios: Usuario[] = [];
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     // Simulação de dados
     this.usuarios = [
       { id: 1, name: 'Alexandre', email: 'alexandre@spezi.com', role: 'ADMIN' },
       { id: 2, name: 'João', email: 'joao@spezi.com', role: 'ADMIN' },
-      { id: 3, name: 'Rodrigo', email: 'rodrigo@spezi.com', role: 'ADMIN' }
+      { id: 3, name: 'Rodrigo', email: 'rodrigo@spezi.com', role: 'ADMIN' },
     ];
   }
+
+  abrirCadastro(): void {
+    this.router.navigate(['/usuarios/novo']);
+  }
+
 }
