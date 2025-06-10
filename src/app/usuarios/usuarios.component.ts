81376040d75dd55f00b65aa54afdc5ebaf5740e2
Alexandre Oliveira Silva <alexandre-silva@spgg.rs.gov.br>
2025-06-10 16:07:23 -0300

    Criação do UsuarioServie e método trazendo usuários do banco

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -5,12 +5,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
+import { UsuarioService } from '../services/usuario.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 
 interface Usuario {
   id: number;
-  name: string;
+  nome: string;
+  username: string;
   email: string;
-  role: string;
+  perfil: string;
+  nomePerfil: string;
+  createdAt: string;
 }
 
 @Component({
@@ -22,27 +30,61 @@ interface Usuario {
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
   ],
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css'],
 })
 export class UsuariosComponent implements OnInit {
-  colunas: string[] = ['id', 'name', 'email', 'role'];
+  colunas: string[] = ['id', 'name', 'email', 'nomePerfil', 'acoes'];
   usuarios: Usuario[] = [];
+  loading = true;
+  error = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private usuarioService: UsuarioService,
+    private snackBar: MatSnackBar
+  ) {
+    console.log('Serviço injetado:', this.usuarioService);
+  }
 
   ngOnInit(): void {
-    // Simulação de dados
-    this.usuarios = [
-      { id: 1, name: 'Alexandre', email: 'alexandre@spezi.com', role: 'ADMIN' },
-      { id: 2, name: 'João', email: 'joao@spezi.com', role: 'ADMIN' },
-      { id: 3, name: 'Rodrigo', email: 'rodrigo@spezi.com', role: 'ADMIN' },
-    ];
+    console.log('Componente inicializado');
+    this.carregarListaUsuarios();
+  }
+
+  carregarListaUsuarios(): void {
+    console.log('Método carregarListaUsuarios chamado');
+    this.usuarioService.getUsuarios().subscribe({
+      next: (data) => {
+        console.log('Dados recebidos:', data);
+        this.usuarios = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Falha ao carregar usuários';
+        this.loading = false;
+        this.mostrarErro('Erro ao carregar usuários');
+        console.error(err);
+      },
+    });
   }
 
   abrirCadastro(): void {
     this.router.navigate(['/usuarios/novo']);
   }
 
+  editarUsuario(id: number): void {
+    this.router.navigate([`/usuarios/editar/${id}`]);
+  }
+
+  private mostrarErro(mensagem: string): void {
+    this.snackBar.open(mensagem, 'Fechar', {
+      duration: 5000,
+      panelClass: ['error-snackbar'],
+    });
+  }
 }
